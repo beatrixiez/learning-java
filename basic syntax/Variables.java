afedf3df10b14924362c6c688cb9d4c41ce469ab
beatriz <100150308+beatrixiez@users.noreply.github.com>
2022-06-22 12:22:32 -0300

    creating variables

---
+++ basic syntax/Variables.java
@@ -0,0 +1,14 @@
+package learningjava;
+
+public class Variables {
+
+    public static void main(String[] args) {
+
+        String nomeDoAluno = "Beatriz Barreto";
+        final String NOME_DO_ALUNO = "Beatriz Barreto";
+
+        System.out.println(nomeDoAluno);
+        System.out.println(NOME_DO_ALUNO);
+    }
+
+}
