04e8d4a6064477931c907ab1bf304b715892d9f6
beatriz <100150308+beatrixiez@users.noreply.github.com>
2022-06-22 14:01:35 -0300

    variables added

--- basic syntax/Variables.java
+++ basic syntax/Variables.java
@@ -4,11 +4,15 @@ public class Variables {
 
     public static void main(String[] args) {
 
-        String nomeDoAluno = "Beatriz Barreto";
         final String NOME_DO_ALUNO = "Beatriz Barreto";
+        int idadeDoAluno = 19;
+        float alturaDoAluno = 1.68f;
+        char genero = 'F';
 
-        System.out.println(nomeDoAluno);
-        System.out.println(NOME_DO_ALUNO);
+        System.out.println("Nome: " + NOME_DO_ALUNO);
+        System.out.println("Idade: " + idadeDoAluno);
+        System.out.println("Altura: " + alturaDoAluno + " metros");
+        System.out.println("Gênero: " + genero);
     }
 
 }
